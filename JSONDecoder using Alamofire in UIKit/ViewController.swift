69aff4b9beca114636c21fcee2650bb400b6c603
Md Khaled Hasan Manna <khmannaict13@gmail.com>
2020-07-07 18:42:12 +0600

    Initial Commit

---
+++ JSONDecoder using Alamofire in UIKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JSONDecoder using Alamofire in UIKit
+//
+//  Created by Md Khaled Hasan Manna on 7/7/20.
+//  Copyright © 2020 Md Khaled Hasan Manna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
